eed98e5adf752f0d35e155f126934504a8e3b722
Mohit Sharma <azuregit@live.com>
2013-06-15 11:52:47 +0530

    V.1

--- firstazureb/register.php
+++ firstazureb/register.php
@@ -38,17 +38,17 @@
     }
 ?>
 <div id="main">
-    <h2>Register an account</h2>
+    <h2>Register an account With Githuband Azure</h2>
         <form action="register.php" method="post">
             <fieldset>
                 <legend>Register an account</legend>
                 <ol>
                     <li>
-                        <label for="username">Username:</label> 
+                        <label for="username">Username of Azure:</label> 
                         <input type="text" name="username" value="" id="username" />
                     </li>
                     <li>
-                        <label for="password">Password:</label>
+                        <label for="password">Password of Azure:</label>
                         <input type="password" name="password" value="" id="password" />
                     </li>
                 </ol>
