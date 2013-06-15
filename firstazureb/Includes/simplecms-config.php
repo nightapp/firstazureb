18d141d382bdc87d91c9a1ea2fba97c87bdd2676
Mohit Sharma <azuregit@live.com>
2013-06-15 11:49:52 +0530

    V0

---
+++ firstazureb/Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms599');
+    define('DB_USER', 'simplecmsuser599');
+    define('DB_PASSWORD', 'aVxS|.k4nbjH');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'azuregit@live.com');
+    define('DEFAULT_ADMIN_PASSWORD', 'soft@123');
+?>
